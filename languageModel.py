79af55da8820012cb9f191d08e11ebdd88be2f79
chunklin <41439810+chunklin@users.noreply.github.com>
2019-03-31 08:49:50 -0700

    Add files via upload

--- languageModel.py
+++ languageModel.py
@@ -1,36 +1,72 @@
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import names
-
+import pyaudio
+import speech_recognition as spreg
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://parkpak-c41e3.firebaseio.com/')
 
 def word_feats(words):
     return dict([(word, True) for word in words])
 
 
-voc_p = ['great', 'fun', 'epic', 'good', ':)']
-voc_n = ['bad', 'terrible', 'help', 'danger', ':(']
+voc_p = ['great', 'fun', 'epic', 'good', 'happy', 'safe', 'normal','amazing' ]
+voc_n = ['bad', 'terrible', 'help', 'danger', 'trouble']
 
 
 ft_pos = [(word_feats(pos), 'pos') for pos in voc_p]
 ft_ng = [(word_feats(neg), 'neg') for neg in voc_n]
 
 
-train_set = ft_ng + ft_pos
+tr_set = ft_ng + ft_pos
 
-classifier = NaiveBayesClassifier.train(train_set)
-
+class_fy = NaiveBayesClassifier.train(tr_set)
 # Predict
-ng = 0
-ps = 0
-sentence = "Awesome movie, I liked it"
-sentence = sentence.lower()
-words = sentence.split(' ')
-for word in words:
-    classResult = classifier.classify(word_feats(word))
-    if classResult == 'neg':
-        ng = ng + 1
-    if classResult == 'pos':
-        ps = ps + 1
-if ps < ng :
-    print('distress')
+def predictNegPos(sentence):
+    ng = 0
+    ps = 0
+
+    sentence = sentence.lower()
+    words = sentence.split(' ')
+    for word in words:
+        classResult = class_fy.classify(word_feats(word))
+        if classResult == 'neg':
+            ng+=1
+        if classResult == 'pos':
+            ps+=1
+    if ng > ps:
+        result = firebase.put(
+            '',
+            '/microphone',
+            {
+                "distress": "distressed!"
+            }
+        )
+        print(result)
+    else:
+        result = firebase.put(
+            '',
+            '/microphone',
+            {
+                "distress": "normal."
+            }
+        )
+        print(result)
+sample_rate = 48000
+data_size = 2048
+
+recog = spreg.Recognizer()
+while 1:
+    with spreg.Microphone(sample_rate = sample_rate, chunk_size = data_size) as source:
+        recog.adjust_for_ambient_noise(source)
+        print('Tell Something: ')
+        speech = recog.listen(source)
+    try:
+        text = recog.recognize_google(speech)
+        print('You have said: ' + text)
+        predictNegPos(text)
+    except spreg.UnknownValueError:
+        print('Unable to recognize the audio')
+    except spreg.RequestError as e:
+        print("Request error from Google Speech Recognition service; {}".format(e))
 
